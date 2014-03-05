e8e85dfc38f1766d05cf7732b13f28113f11a78e
Bela Markus <bmarkus@hasix.org>
2014-03-05 21:19:09 +0000

    Fix typo and module info

--- compile_ax25.py
+++ compile_ax25.py
@@ -4,8 +4,7 @@ from distutils.core import setup, Extension
 
 module1 = Extension('_ax25', libraries = ['ax25', 'ax25io'], sources = ['_ax25module.c'])
 
-setup (name = 'PackageName',
+setup (name = 'pyax25',
         version = '1.0',
-        description = 'Wrapping for LINUX ax.25 stack',
+        description = 'Wrapper for LINUX ax.25 stack',
         ext_modules = [module1])
-        
