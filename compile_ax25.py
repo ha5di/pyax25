f73c7310dfd5f5ddbf6424f5fc44668c36cb9bee
Bela Markus <bmarkus@hasix.org>
2014-03-06 06:35:27 +0000

    Fixed egg-info content

--- compile_ax25.py
+++ compile_ax25.py
@@ -5,6 +5,12 @@ from distutils.core import setup, Extension
 module1 = Extension('_ax25', libraries = ['ax25', 'ax25io'], sources = ['_ax25module.c'])
 
 setup (name = 'pyax25',
-        version = '1.0',
+        version = '1.1',
+        author = 'Bela Markus, HA5DI',
+        author_email = 'ha5di@hasix.org',
+        platforms = ['Linux'],
+        url = 'http://sites.google.com/site/dixprs/',
         description = 'Wrapper for LINUX ax.25 stack',
+        long_description = 'Wrapper for LINUX ax.25 stack',
+        license = 'GPL v2',
         ext_modules = [module1])
